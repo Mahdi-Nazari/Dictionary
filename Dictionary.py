2555e37fb9ad08313df0557f1a7f30243594b47e
Mahdi-Nazari <Mahdinazari830@gmail.com>
2020-04-04 18:49:21 +0430

    Dictionary created

---
+++ Dictionary.py
@@ -0,0 +1,23 @@
+word = input("Enter your word")
+if word=="hello":
+	print("سلام")
+elif word=="goodbye":
+	print("خداحافظ")
+elif word=="enter":
+	print("وارد کردن")
+elif word=="apple":
+	print("سیب")
+elif word=="pen":
+	print("خودکار")
+elif word=="pencil":
+	print("مداد")
+elif word=="eraser":
+	print("پاک کن")
+elif word=="folder":
+	print("پوشه")
+elif word=="music":
+	print("آهنگ")
+elif word=="book":
+	print("کتاب")
+else: 
+	print("Your word does not found")
